395a0b134be3c8e9f4921806d8eebc12f34f5d20
egfx <egfx@users.noreply.github.com>
2016-06-26 00:11:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -401,7 +401,7 @@
 	function disableTilt() {
 		classie.add(tiltCtrl, 'action--disabled');
 		tilt = false;
-		disableTilt();
+		classie.remove(tiltCtrl, 'action--shown');
 	}
 
 	function enableTilt() {
