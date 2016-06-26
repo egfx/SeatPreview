ddae6c6ff32a8548284681ca4a427dbbb5c8c0b6
egfx <egfx@users.noreply.github.com>
2016-06-25 21:24:16 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -267,10 +267,10 @@
 			classie.toggle(playCtrl, 'action--faded');
 			scaleRoom();
 			applyRoomTransform({'translateX' : 0, 'translateY' : 0, 'translateZ' : 0, 'rotateX' : 0, 'rotateY' : 0});
+			applyRoomTransform({'translateZ' : 1300});
 			zoomOutScreen(function() {
-				applyRoomTransform({'translateZ' : 1300});
 				disableTilt();
-				//video.removeEventListener('click', null);
+				video.removeEventListener('click', null);
 			});
 		}
 		
