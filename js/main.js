7551cf3aace4c5dce6dd937b4c3300435b65eb9b
egfx <egfx@users.noreply.github.com>
2016-06-26 12:12:46 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -258,23 +258,26 @@
 			classie.add(playCtrl, 'action--faded');
 			zoomOutScreen(function() {
 				showTiltCtrl();
+				window.isReset = false;
 			}); 
 		};
 		
 		var onSelectScreen = function(event) {
-			classie.toggle(intro, 'intro--shown');
-			classie.toggle(plan, 'plan--shown');
-			classie.toggle(playCtrl, 'action--faded');
-			scaleRoom();
-			applyRoomTransform({'translateX' : 0, 'translateY' : 0, 'translateZ' : 1300, 'rotateX' : 0, 'rotateY' : 0});
-			disableTilt();
-			event.target.removeEventListener('click', null);
-			//window.setTimeout( function(){ applyRoomTransform({'translateZ' : 1300}); }, 3000);
-			/*zoomOutScreen(function() {
-				applyRoomTransform({'translateZ' : 1300});
+			if(!window.isReset) {
+				classie.toggle(intro, 'intro--shown');
+				classie.toggle(plan, 'plan--shown');
+				classie.toggle(playCtrl, 'action--faded');
+				scaleRoom();
+				applyRoomTransform({'translateX' : 0, 'translateY' : 0, 'translateZ' : 1300, 'rotateX' : 0, 'rotateY' : 0});
 				disableTilt();
-				video.removeEventListener('click', null);
-			});*/
+				window.isReset = true;
+				//window.setTimeout( function(){ applyRoomTransform({'translateZ' : 1300}); }, 3000);
+				/*zoomOutScreen(function() {
+					applyRoomTransform({'translateZ' : 1300});
+					disableTilt();
+					video.removeEventListener('click', null);
+				});*/
+			}
 		}
 		
 		selectSeatsCtrl.addEventListener('click', onSelectSeats);
